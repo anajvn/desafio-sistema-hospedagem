d9bf75f3e70fc0af2738a454bd5726a30b2ac59a
Ana Julia Neiverth <115558556+anajvn@users.noreply.github.com>
2023-10-30 20:11:35 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -21,5 +21,19 @@ reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}"); // Esperado: 2
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria().ToString("C2")}"); // Esperado: 150,00
+
+// Cria uma nova reserva, com um numero de dias igual a 10
+Reserva reserva2 = new Reserva(diasReservados: 10);
+reserva2.CadastrarSuite(suite);
+reserva2.CadastrarHospedes(hospedes);
+
+// Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine($"Hóspedes: {reserva2.ObterQuantidadeHospedes()}"); // Esperado: 2
+Console.WriteLine($"Valor diária: {reserva2.CalcularValorDiaria().ToString("C2")}"); // Esperado: 270,00
+
+// Cria uma nova reserva, com um numero de hospedes maior que a capacidade da suite
+Reserva reserva3 = new Reserva(diasReservados: 5);
+reserva3.CadastrarSuite(new Suite(tipoSuite: "Premium Single", capacidade: 1, valorDiaria: 30));
+reserva3.CadastrarHospedes(hospedes); // Esperado: Exceção
